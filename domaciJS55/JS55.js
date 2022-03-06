6247469c20f00199b18017a54c0d974b295d904f
Miljan Cosovic <miljancosovicnp@gmail.com>
2022-03-06 21:15:59 +0100

    Examples with callbakc in JavaScript

--- domaciJS55/JS55.js
+++ domaciJS55/JS55.js
@@ -1,13 +1,23 @@
 
-function a(arg)
-{
-  console.log("Neki kod");
-  console.log("nesto");
-  arg();
+// function a(arg)
+// {
+//   console.log("Neki kod");
+//   console.log("nesto");
+//   arg();
+// }
+
+// function hello()
+// {
+//   console.log("Hello wolrdd");
+// }
+// a(hello);
+function myDisplayer(some) {
+  document.getElementById("demo").innerHTML = some;
 }
 
-function hello()
-{
-  console.log("Hello wolrdd");
+function myCalculator(num1, num2) {
+  let sum = num1 + num2;
+  myDisplayer(sum);
 }
-a(hello);
+
+myCalculator(5, 5);
