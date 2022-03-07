64bfdcfb65ada4f87ab4c2584455178d804df179
Miljan Cosovic <miljancosovicnp@gmail.com>
2022-03-07 19:14:39 +0100

    Examples with callbakc in JavaScript

--- domaciJS55/JS55.js
+++ domaciJS55/JS55.js
@@ -10,14 +10,31 @@
 // {
 //   console.log("Hello wolrdd");
 // }
-// a(hello);
-function myDisplayer(some) {
-  document.getElementById("demo").innerHTML = some;
+// // a(hello);
+// function myDisplayer(some) {
+//   document.getElementById("demo").innerHTML = some;
+// }
+
+// function myCalculator(num1, num2) {
+//   let sum = num1 + num2;
+//   myDisplayer(sum);
+// }
+
+// myCalculator(5, 5);
+
+
+function greet(name, myFunction) {
+  console.log('Hello world');
+
+  // callback function
+  // executed only after the greet() is executed
+  myFunction(name);
 }
 
-function myCalculator(num1, num2) {
-  let sum = num1 + num2;
-  myDisplayer(sum);
+// callback function
+function sayName(name) {
+  console.log('Hello' + ' ' + name);
 }
 
-myCalculator(5, 5);
+// calling the function after 2 seconds
+setTimeout(greet, 2000, 'John', sayName);
