04b929570f48430292d5d57a9ebaebce3faa3908
Miljan Cosovic <miljancosovicnp@gmail.com>
2022-03-08 18:44:54 +0100

    Examples with promise in JavaScript

--- domaciJS55/JS55.js
+++ domaciJS55/JS55.js
@@ -23,18 +23,48 @@
 // myCalculator(5, 5);
 
 
-function greet(name, myFunction) {
-  console.log('Hello world');
+// function greet(name, myFunction) {
+  // console.log('Hello world');
 
   // callback function
   // executed only after the greet() is executed
-  myFunction(name);
-}
+  // myFunction(name);
+// }
 
 // callback function
-function sayName(name) {
-  console.log('Hello' + ' ' + name);
-}
+// function sayName(name) {
+  // console.log('Hello' + ' ' + name);
+// }
 
 // calling the function after 2 seconds
-setTimeout(greet, 2000, 'John', sayName);
+// setTimeout(greet, 2000, 'John', sayName);
+
+
+
+
+
+
+
+
+
+
+
+
+async function getData()
+{
+  try
+  {
+    const res=await fetch("https://jsonplaceholder.typicode.com/users");
+    const resJson=await res.json()
+    const name= resJson.map((el) =>{
+      return el.name;
+    })
+    console.log(name);
+    
+  }catch(err)
+  {
+    console.log("Problem sa serverom");
+  }
+}
+
+getData()
