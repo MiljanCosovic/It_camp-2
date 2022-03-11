1b9a25a39a9413ec5221c502b43f03875fb457e8
Miljan Cosovic <miljancosovicnp@gmail.com>
2022-03-11 12:28:25 +0100

    Examples with DOM in JavaScript

--- domaciJS55/JS55.js
+++ domaciJS55/JS55.js
@@ -63,11 +63,48 @@
 
 // getData()
 
-var novi = document.createElement('div');
-var container1=document.getElementById("container") 
-novi.style.border='1px solid black';
-novi.style.width='300px';
-novi.style.height='300px';
-novi.style.backgroundColor='red';
-container1.appendChild(novi);
+// var novi = document.createElement('div');
+// var container1=document.getElementById("container") 
+// novi.style.border='1px solid black';
+// novi.style.width='300px';
+// novi.style.height='300px';
+// novi.style.backgroundColor='red';
+// novi.style.display='flex';
+// novi.style.alignItems='center';
+// novi.style.justifyContent='center';
+// container1.appendChild(novi);
+
+
+function generateCard(vals)
+{
+  var subEl=document.createElement('div');
+  subEl.style.backgroundColor='green';
+  subEl.style.border='1 px solid black';
+  subEl.style.width='300px';
+  subEl.style.height='400px';
+  subEl.style.margin='10px';
+  subEl.innerHTML=vals.fact;
+  return subEl
+
+}
+
+var ctr=document.getElementById('container');
+ctr.style.display='flex';
+ctr.style.justifyContent='flex-start';
+ctr.style.flexWrap='wrap';
+
+
+fetch("https://catfact.ninja/facts")
+.then((res) =>{
+  return res.json();
+})
+.then((res) =>{
+  for(let i=0; i<res.data.length;i++)
+  {
+    ctr.appendChild(generateCard(res.data[i]))
+  }
+})
+.catch((err) =>{
+  console.log(err);
+})
 
