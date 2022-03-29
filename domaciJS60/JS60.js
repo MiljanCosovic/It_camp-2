496c99157bc31eb886db776cfaf2e17834807d35
Miljan Cosovic <miljancosovicnp@gmail.com>
2022-03-29 21:38:47 +0200

    Examples with arrays in JavaScript

---
+++ domaciJS60/JS60.js
@@ -0,0 +1,30 @@
+var niz1 = [3,11,2,45,2,1234,2,3];
+var niz2 = ["oke",33354,"java", "script"];
+// var niz= niz1.entries(niz2)
+// console.log(niz);
+
+// function proveri(god)
+// {
+//     return god > 18
+// }
+
+// console.log(niz1.every(proveri));
+
+
+// niz1.fill("oke",0,4);
+// console.log(niz1);
+
+
+// pom =niz1.filter(( el,index) => niz1.indexOf(el) === index )
+// console.log(pom);
+
+
+function getMostFrequent(arr) {
+    const hashmap = arr.reduce( (acc, val) => {
+     acc[val] = (acc[val] || 0 ) + 1
+     return acc
+  },{})
+ console.log(Object.keys(hashmap).reduce((a, b) => hashmap[a] > hashmap[b] ? a : b)); 
+ }
+
+ getMostFrequent([1,44,2,11,2,4,2])
