5f04b69c02cee1b83e30ededc7cb76aea19c45a4
Miljan Cosovic <miljancosovicnp@gmail.com>
2022-07-14 15:39:54 +0200

    Project

---
+++ my-app/src/App.jsx
@@ -0,0 +1,10 @@
+import Card from './Card'
+const App = () =>{
+  return <div className="container">
+   <Card />
+   <Card />
+   <Card />
+  </div>
+}
+
+export default App;
---
+++ my-app/src/Card.jsx
@@ -0,0 +1,8 @@
+const card = () =>{
+    return <div style={{border:"1px solid red",background:"blue", margin:"10px"}}>
+         <h1>Ovo je paragraf</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, quo distinctio repellendus aliquid corrupti aliquam praesentium illo. Autem atque, iure ea quibusdam nam dolor quam asperiores cumque harum eligendi rerum.</p>
+    </div>
+}
+
+export default card
