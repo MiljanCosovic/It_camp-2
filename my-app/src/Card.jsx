4d7b6d1f3afce4f34135b9456e9d56c63181a86f
Miljan Cosovic <miljancosovicnp@gmail.com>
2022-07-15 19:31:03 +0200

    Project

--- my-app/src/Card.jsx
+++ my-app/src/Card.jsx
@@ -1,5 +1,5 @@
 const card = () =>{
-    return <div style={{border:"1px solid red",background:"blue", margin:"10px"}}>
+    return <div style={{border:"1px solid red",background:"lightblue", margin:"10px"}}>
          <h1>Ovo je paragraf</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, quo distinctio repellendus aliquid corrupti aliquam praesentium illo. Autem atque, iure ea quibusdam nam dolor quam asperiores cumque harum eligendi rerum.</p>
     </div>
